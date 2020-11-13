651b5984fb838368457a51be9c36ec4e3c2bcef5
basiliuswicaksono <basilius.wicaksono@gmail.com>
2020-11-13 17:15:29 +0800

    update minor

--- server/routes/QueueRoute.js
+++ server/routes/QueueRoute.js
@@ -1,13 +1,15 @@
 const router = require('express').Router()
 const QueueController = require('../controllers/QueueController')
-const { customerAuthentication } = require('../middlewares/authentication')
+const { customerAuthentication, adminAuthentication } = require('../middlewares/authentication')
+const { adminAuthorization } = require('../middlewares/authorization')
 
 
 router.get('/', customerAuthentication, QueueController.getQueue)
 router.post('/', customerAuthentication, QueueController.addQueue)
 
 //authorisasi dari Outlet
-router.put('/:id', QueueController.updateQueue)
-router.delete('/:id', QueueController.deleteQueue)
+router.use(adminAuthentication)
+router.put('/:id', adminAuthorization, QueueController.updateQueue)
+router.delete('/:id', adminAuthorization, QueueController.deleteQueue)
 
 module.exports = router
