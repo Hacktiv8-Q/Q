96a687a032c54cafa5887e1610e95c385fe00829
wachidmudi <wachidmudi@gmail.com>
2020-11-17 09:33:33 +0700

    Add env dev or prod

--- server/firebaseInit.js
+++ server/firebaseInit.js
@@ -1,22 +1,24 @@
-// const fs = require('fs');
-// const path = require('path');
 const admin = require('firebase-admin');
 
-// Prod env
-// let firebaseKey = process.env.FIREBASE_CREDENTIALS
+let firebaseCredential = process.env.FIREBASE_CREDENTIALS
 
-// Dev env
-// if (path.extname(process.env.FIREBASE_CREDENTIALS) === '.json') {
-//   firebaseKey = fs.readFileSync(process.env.FIREBASE_CREDENTIALS, 'utf8')
-// }
-
-admin.initializeApp({
-  credential: admin.credential.cert({
+if (process.env.NODE_ENV === "production") {
+  firebaseCredential = {
     "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
     "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-  }),
+    "type": process.env.FIREBASE_TYPE,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT,
+  }
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseCredential),
   databaseURL: 'https://q-wapp.firebaseio.com'
 });
 
