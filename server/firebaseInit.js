cd64a418d62520db2a5d54af54ba33b44ace8077
wachidmudi <wachidmudi@gmail.com>
2020-11-17 08:21:59 +0700

    Add project_id to obect keys firebase

--- server/firebaseInit.js
+++ server/firebaseInit.js
@@ -12,6 +12,7 @@ const admin = require('firebase-admin');
 
 admin.initializeApp({
   credential: admin.credential.cert({
+    "project_id": process.env.FIREBASE_PROJECT_ID,
     "private_key": process.env.FIREBASE_PRIVATE_KEY,
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   }),
