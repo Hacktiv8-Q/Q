6b0aa5d60a890a5826f1ba95e57032ad27a808e4
wachidmudi <wachidmudi@gmail.com>
2020-11-17 09:57:18 +0700

    Fix json parse

--- server/firebaseInit.js
+++ server/firebaseInit.js
@@ -1,7 +1,17 @@
+const fs = require('fs');
+const path = require('path');
 const admin = require('firebase-admin');
 
+// Prod env
+let firebaseKey = process.env.FIREBASE_CREDENTIALS
+
+// Dev env
+if (process.env.NODE_ENV === "production") {
+  firebaseKey = JSON.parse(fs.readFileSync(process.env.FIREBASE_CREDENTIALS, 'utf8').replace(/\\n/g, '\n'))
+}
+
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CREDENTIALS)),
+  credential: admin.credential.cert(firebaseKey),
   databaseURL: 'https://q-wapp.firebaseio.com'
 });
 
