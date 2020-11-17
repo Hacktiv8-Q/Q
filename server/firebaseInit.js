3c3c6b2825f31cddcf510006960c1aa7c103a115
wachidmudi <wachidmudi@gmail.com>
2020-11-17 10:53:06 +0700

    Try to deploy on ec2

--- server/firebaseInit.js
+++ server/firebaseInit.js
@@ -1,17 +1,18 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 const admin = require('firebase-admin');
 
 // Prod env
-let firebaseKey = process.env.FIREBASE_CREDENTIALS
+// let firebaseKey = process.env.FIREBASE_CREDENTIALS
 
 // Dev env
-if (process.env.NODE_ENV === "production") {
-  firebaseKey = JSON.parse(fs.readFileSync(process.env.FIREBASE_CREDENTIALS, 'utf8').replace(/\\n/g, '\n'))
-}
+// if (process.env.NODE_ENV === "production") {
+// const data = fs.readFileSync(process.env.FIREBASE_CREDENTIALS, 'utf8')
+// firebaseKey = JSON.parse(data)
+// }
 
 admin.initializeApp({
-  credential: admin.credential.cert(firebaseKey),
+  credential: admin.credential.cert(process.env.FIREBASE_CREDENTIALS),
   databaseURL: 'https://q-wapp.firebaseio.com'
 });
 
