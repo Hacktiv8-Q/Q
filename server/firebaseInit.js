bda0a7ff5fd69d9511e6a0a477bb551d78a91402
wachidmudi <wachidmudi@gmail.com>
2020-11-17 08:17:58 +0700

    Trim firebase config keys

--- server/firebaseInit.js
+++ server/firebaseInit.js
@@ -1,17 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 const admin = require('firebase-admin');
 
 // Prod env
-let firebaseKey = process.env.FIREBASE_CREDENTIALS
+// let firebaseKey = process.env.FIREBASE_CREDENTIALS
 
 // Dev env
-if (path.extname(process.env.FIREBASE_CREDENTIALS) === '.json') {
-  firebaseKey = fs.readFileSync(process.env.FIREBASE_CREDENTIALS, 'utf8')
-}
+// if (path.extname(process.env.FIREBASE_CREDENTIALS) === '.json') {
+//   firebaseKey = fs.readFileSync(process.env.FIREBASE_CREDENTIALS, 'utf8')
+// }
 
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(firebaseKey)),
+  credential: admin.credential.cert({
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  }),
   databaseURL: 'https://q-wapp.firebaseio.com'
 });
 
